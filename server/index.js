b25143d06aff7ea3678a34b55904f1af061341b0
ramMahender16 <ram.mahender@gmail.com>
2026-06-17 19:24:18 +0530

    fix Socket.IO CORS allow all origins

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const allowedOrigins = [
 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
